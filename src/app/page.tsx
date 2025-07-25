b32e1a4abeba2a63cf5810e846f0540ee334cf0e
Malaiarasu G <govindarajmalaiarasu@gmail.com>
2025-07-25 03:50:57 +0000

    User Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import { LoginForm } from '@/components/login-form';
-import { LoginImage } from '@/components/login-image';
 
 export default function LoginPage() {
   return (
-    <main className="flex min-h-screen w-full flex-wrap items-center justify-center">
-      <LoginImage />
-      <div className="w-full p-4 lg:w-1/2 flex items-center justify-center">
-        <div className="w-full max-w-md">
-          <LoginForm />
-        </div>
+    <main className="flex min-h-screen w-full items-center justify-center">
+      <div className="w-full max-w-md p-4">
+        <LoginForm />
       </div>
     </main>
   );
